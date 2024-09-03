57bd14f471fcd73c3b03668cd9fa292af79feaf5
rauelen <129184726+rauelen@users.noreply.github.com>
2024-09-03 13:18:18 -0300

    dfygdrfbdf

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-const elemento = document.querySelector(".teste");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+
+const perguntas = [
+    {
+        enunciado: "Qual a data da independênica do Brasil?",
+        alternativas: ["07 de Setembro","15 de Novembro","21 de Abril"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+
+
+];
+
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+perguntaAtual =  perguntas[atual];
+caixaPergunta.textContent = perguntaAtual.enunciado;
+
+}
+
+mostraPergunta();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
